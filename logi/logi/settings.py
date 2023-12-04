f577a15596131d4ade0b0fd2c9714e2c443edf2a
Casey24 <caseykinsley77@gmail.com>
2023-12-04 14:59:13 -0500

    static settings

--- logi/logi/settings.py
+++ logi/logi/settings.py
@@ -125,15 +125,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', "staticfiles")
 
 MEDIA_URL = '/images/'
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
