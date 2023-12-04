8fce4c90413a4a2a41b7ee0eb5bcabfc75cc7c52
Casey24 <caseykinsley77@gmail.com>
2023-12-04 14:07:45 -0500

    vercel config

--- logi/logi/settings.py
+++ logi/logi/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =SECRET_KEY = config('THE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
